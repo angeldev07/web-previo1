739727ba44dd0a6cbe0cc83879705445d8e49c6b
angeldev07 <angelgaran20@gmail.com>
2022-10-06 23:07:57 -0500

    Terminado el carrito de compras. Fin del primer previo

--- js/carrito.js
+++ js/carrito.js
@@ -1,21 +1,60 @@
-const productosContainer = document.querySelector(".products")
+const productosContainer = document.querySelector(".products");
+
+function keysLs() {
+  const keys = Object.keys(localStorage);
+  return keys;
+}
 
 function showProductos() {
   //obtenemos las keys del  localstorage
-  const keys = Object.keys(localStorage);
-  keys.forEach((key) => {
-    const producto = JSON.parse(localStorage.getItem(key))
+  const keys = keysLs();
+
+  keys.forEach((key, index) => {
+    const producto = JSON.parse(localStorage.getItem(key));
     productosContainer.innerHTML += `
+    <div class="product">
     <div class="img-product">
         <img src="${producto.img}" alt="nada">
     </div>
     <div class="product-info">
         <h2 class="product-name">${producto.name}</h2>
-        <p class="precio">${producto.precio}</p>
-        <input type="number" name="cantidad" id="cantidad" min="1">
+        <p class="precio">${producto.value}</p>
+        <input class="cantidad" type="number" name="cantidad"  min="1" value="1">
     </div>
-`
+    <button type="button" class="btn btn-danger delete"><span class="material-symbols-outlined">
+    delete
+    </span></button>
+    </div >
+`;
   });
+  const elementsPrecio = [...document.querySelectorAll(".precio")];
+  total(elementsPrecio);
+
+  [...document.querySelectorAll(".cantidad")].forEach((btn, index) =>
+    btn.addEventListener("click", (e) => {
+      elementsPrecio[index].innerHTML = 20000 * e.target.value;
+      total(elementsPrecio)
+    })
+  );
+
+  // Borrar elementos
+   [...document.querySelectorAll(".delete")].forEach((del, index) =>
+    del.addEventListener("click", (e) => {
+      localStorage.removeItem(keys[index]);
+      document
+        .querySelector(".products")
+        .removeChild(document.querySelector(".products").children[index]);
+        const full = total(elementsPrecio)
+        full.innerHTML =  (parseInt(full.innerHTML)-parseInt(elementsPrecio[index].innerHTML))
+    })
+  );
+  
 }
 
-showProductos()
+function total(precios) {
+  const totalContainer = document.querySelector('.precioTotal')
+  totalContainer.innerHTML = precios.reduce((total, precio) => total + parseInt(precio.innerHTML), 0)
+  return totalContainer
+}
+
+showProductos();
